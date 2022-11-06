048f4563d758fd67d9f058495f8a455157a9b5ec
eidovolta <19731799+eidovolta@users.noreply.github.com>
2022-11-06 16:50:33 +0100

    Add add,subtract,multiply,divide

---
+++ script.js
@@ -0,0 +1,39 @@
+/**
+ * Add two numbers
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns result number of both numbers added together
+ */
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+/**
+ * Subtract num2 from num1
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns result number of num2 subtracted from num1
+ */
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+/**
+ * Multiply both numbers together
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns result number of both numbers multiplied together
+ */
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+/**
+ * Divide num1 by num2
+ * @param {number} num1 
+ * @param {number} num2 
+ * @returns result number of num1 divided by num2
+ */
+function divide (num1, num2) {
+    return num1 / num2;
+}
